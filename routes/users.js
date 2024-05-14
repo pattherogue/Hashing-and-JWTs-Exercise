7d74156110d38d681ee9aef2cb594d2b138f9ec3
Paolo Gomez <peweegardner248@gmail.com>
2024-05-13 18:59:21 -0700

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,33 @@
+const express = require("express");
+const User = require("../models/user");
+const { ensureLoggedIn, ensureCorrectUser } = require("../middleware/auth");
+const router = new express.Router();
+
 /** GET / - get list of users.
  *
  * => {users: [{username, first_name, last_name, phone}, ...]}
- *
  **/
+router.get("/", ensureLoggedIn, async function (req, res, next) {
+  try {
+    const users = await User.all();
+    return res.json({ users });
+  } catch (err) {
+    return next(err);
+  }
+});
 
-
-/** GET /:username - get detail of users.
+/** GET /:username - get detail of user.
  *
  * => {user: {username, first_name, last_name, phone, join_at, last_login_at}}
- *
  **/
-
+router.get("/:username", ensureLoggedIn, ensureCorrectUser, async function (req, res, next) {
+  try {
+    const user = await User.get(req.params.username);
+    return res.json({ user });
+  } catch (err) {
+    return next(err);
+  }
+});
 
 /** GET /:username/to - get messages to user
  *
@@ -19,9 +36,15 @@
  *                 sent_at,
  *                 read_at,
  *                 from_user: {username, first_name, last_name, phone}}, ...]}
- *
  **/
-
+router.get("/:username/to", ensureLoggedIn, ensureCorrectUser, async function (req, res, next) {
+  try {
+    const messages = await User.messagesTo(req.params.username);
+    return res.json({ messages });
+  } catch (err) {
+    return next(err);
+  }
+});
 
 /** GET /:username/from - get messages from user
  *
@@ -30,5 +53,14 @@
  *                 sent_at,
  *                 read_at,
  *                 to_user: {username, first_name, last_name, phone}}, ...]}
- *
  **/
+router.get("/:username/from", ensureLoggedIn, ensureCorrectUser, async function (req, res, next) {
+  try {
+    const messages = await User.messagesFrom(req.params.username);
+    return res.json({ messages });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+module.exports = router;
